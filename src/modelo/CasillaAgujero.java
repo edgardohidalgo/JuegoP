38fce78401a72acc222b81b81845e459fa433d80
Edgardo Hidalgo <jugador.eo@gmail.com>
2025-05-09 22:17:04 +0200

    Reapply "Merge pull request #2 from edgardohidalgo/Edgardo"
    This reverts commit 182c57fa3c6aaae3fbc8e93a2b336beeaae84d72.

--- src/modelo/CasillaAgujero.java
+++ src/modelo/CasillaAgujero.java
@@ -3,12 +3,18 @@ package modelo;
 import java.util.ArrayList;
 
 public class CasillaAgujero extends Casilla {
+
     public CasillaAgujero(int idCasilla ,int posicion , ArrayList<Jugador> jugadoresActuales) {
         super(idCasilla, posicion , jugadoresActuales);
     }
+
     @Override
-    public void realizarAccion() {
-
+    public int realizarAccion(Jugador jugador) {
+        if (jugador instanceof Pinguino) {
+            int nuevaPos = Math.max(jugador.getPosicion() - 1, 0); // Asegura que no se mueva fuera del tablero
+            jugador.setPosicion(nuevaPos);
+            return nuevaPos;
+        }
+        return jugador.getPosicion();
     }
-
 }
